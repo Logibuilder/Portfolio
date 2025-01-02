44f7e5141c7713d13e6e0cda4a589bb21a0a1576
Assane KANE <assane.kane@etu.univ-tlse2.fr>
2025-01-03 00:06:56 +0100

    mon portfolio mise à jour

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ document.querySelector("#cv_btn").addEventListener('click', () => {
     link.download = 'cv_Assane_KANE.pdf';  // Le nom du fichier qui sera téléchargé
     link.click();  // Déclenche le téléchargement
 });
+
+
+window.onload = function() {
+    // Affiche la modale lorsque la page est chargée
+    const modal = document.querySelector('.en-developpement');
+    const closeButton = document.querySelector('.fermer');
+    
+    // Afficher la modale
+    modal.style.display = "flex";
+
+    // Fermer la modale au clic sur le bouton "Fermer"
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+};
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    nom_element = document.querySelector("#nom-prenom");
+    const nom = "Assane KANE";
+    let index = 0;
+
+    function ecrireParLettre()  {
+        if (index < nom.length  ) {
+            nom_element.innerHTML += nom[index];// Ajouter une lettre
+            index++;
+            setTimeout(ecrireParLettre, 100); // Délai de 100ms entre chaque lettre
+        }
+    }
+
+    ecrireParLettre(); // Appel de la fonction pour commencer à écrire
+});
